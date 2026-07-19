ebfad5198c95001bc6a4771ebb5078d4ccff3c90
helpalifshenltd-maker <help.alifshen.ltd@gmail.com>
2026-07-19 07:38:24 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "my.alifbooldbank.com"
     minSdk = 21
     targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "2.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
